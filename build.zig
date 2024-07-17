b0da4b210abde1148a0321cfce2dfcd3bb8899e0
Fillipe Meireles <fillipemeireles@Fillipes-MacBook-Air.local>
2024-07-17 12:29:04 -0300

    updating build.zig

--- build.zig
+++ build.zig
@@ -3,15 +3,6 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardOptimizeOption(.{});
-
-    //const sdl2 = b.addStaticLibrary(.{
-    //   .name = "sdl2",
-    //  .target = target,
-    // .optimize = mode,
-    // });
-    //sdl2.linkLibC();
-    //sdl2.linkSystemLibrary("sld2");
-
     const exe = b.addExecutable(.{
         .name = "breakout",
         .target = target,
